ad05e97ff8e44be97edc718cbd3884eb6891a58d
mickaelaustoni <mickael.austoni@gmail.com>
2022-11-22 17:12:03 +0100

    change repo name

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
 # [Versions](https://github.com/Tracktor/eslint-config-react-tracktor/releases)
 
-## v0.0.4
-- Fix repository path
+## v0.0.5
+- Change repository name
--- bin/create.js
+++ bin/create.js
@@ -12,7 +12,7 @@ const repository = "https://github.com/Tracktor/boilerplate-vite-react-ts";
 if (process.argv.length < 3) {
   console.log("\x1b[31m", "You have to provide name to your app.");
   console.log("For example:");
-  console.log("    npx create-boilerplate-vite-react-ts my-app", "\x1b[0m");
+  console.log("    npx boilerplate-vite-react-ts my-app", "\x1b[0m");
   process.exit(1);
 }
 
