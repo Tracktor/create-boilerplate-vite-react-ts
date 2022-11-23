3ec9702df6b6fc4da2cb3b2e69a392146ec68c58
mickaelaustoni <mickael.austoni@gmail.com>
2022-11-23 11:32:51 +0100

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    coverage: {
+      exclude: ["node_modules/", "src/config/setupTests.ts"],
+      reporter: ["text", "html"],
+    },
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "src/config/setupTests.ts",
+  },
 });
