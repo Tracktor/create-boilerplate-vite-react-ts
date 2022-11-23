ad958c2cf48163f25463e464cb330e54d400ea53
mickaelaustoni <mickael.austoni@gmail.com>
2022-11-23 11:35:29 +0100

    config alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "src") },
+      { find: "~", replacement: resolve(__dirname) },
+    ],
+  },
   test: {
     coverage: {
       exclude: ["node_modules/", "src/config/setupTests.ts"],
